52e910abbe4b49c97d01059a73c921de25552a37
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-03-13 18:44:19 +0100

    Heading component test 10%

---
+++ src/components/Heading/Heading.test.jsx
@@ -0,0 +1,35 @@
+import { Heading } from './';
+import { renderTheme } from '../../styles/render-theme';
+import { screen } from '@testing-library/react';
+import { theme } from '../../styles/theme';
+
+describe('<Heading />', () => {
+  it('should render with default values', () => {
+    renderTheme(<Heading>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.primaryColor,
+      'font-size': theme.font.sizes.xlarge,
+      'text-transform': 'none',
+    });
+  });
+
+  it('should render with white color', () => {
+    renderTheme(<Heading colorDark={false}>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.white,
+    });
+  });
+
+  it('should render correct heading size', () => {
+    renderTheme(<Heading size="small">texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      'font-size': theme.font.sizes.medium,
+    });
+  });
+});
