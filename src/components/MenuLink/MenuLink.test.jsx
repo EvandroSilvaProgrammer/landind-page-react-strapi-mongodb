07ac4961d4b81f620d34a419ccba58b496236b1b
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-03-20 18:23:33 +0100

    Created MenuLink Component and you stories

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,10 @@
+const { screen } = require('@testing-library/react');
+import { renderTheme } from '../../styles/render-theme';
+import { MenuLink } from '.';
+
+describe('<MenuLink />', () => {
+  it('should render', () => {
+    renderTheme(<MenuLink>Children</MenuLink>);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
