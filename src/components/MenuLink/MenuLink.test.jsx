e2ef29aeb04d61e291c82397ca59d650499b46b5
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-03-20 19:45:56 +0100

    Created test for MenuLink Component

--- src/components/MenuLink/MenuLink.test.jsx
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -3,8 +3,32 @@ import { renderTheme } from '../../styles/render-theme';
 import { MenuLink } from '.';
 
 describe('<MenuLink />', () => {
-  it('should render', () => {
-    renderTheme(<MenuLink>Children</MenuLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">Children</MenuLink>);
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={false}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toMatchSnapshot();
   });
 });
