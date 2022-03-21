7637623df238f15009ce7d1c2623cf9d0272e62b
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-03-21 22:25:13 +0100

    Created LogoLink component

--- src/components/LogoLink/LogoLink.test.jsx
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -3,8 +3,31 @@ import { renderTheme } from '../../styles/render-theme';
 import { LogoLink } from '.';
 
 describe('<LogoLink />', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>Children</LogoLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Logo text" />);
+    expect(
+      screen.getByRole('heading', { name: 'Logo text' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Logo text' })).toHaveAttribute(
+      'href',
+      '#target',
+    );
+  });
+
+  it('should render image logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Logo text" srcImg="path/logo.svg" />,
+    );
+    expect(screen.getByRole('img', { name: 'Logo text' })).toHaveAttribute(
+      'src',
+      'path/logo.svg',
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <LogoLink link="#target" text="Logo text" srcImg="path/logo.svg" />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
--- src/components/LogoLink/styles.js
+++ src/components/LogoLink/styles.js
@@ -1,5 +1,13 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  ${({ theme }) => css``}
+export const Container = styled.a`
+  ${({ theme }) => css`
+    text-decoration: none;
+    align-items: center;
+    color: inherit;
+
+    > img {
+      max-height: 3rem;
+    }
+  `}
 `;
