a41a729ae34d2f342c4d885efd36b3d710ca830c
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-03-21 21:33:17 +0100

    Init LogoLink component

---
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -0,0 +1,10 @@
+const { screen } = require('@testing-library/react');
+import { renderTheme } from '../../styles/render-theme';
+import { LogoLink } from '.';
+
+describe('<LogoLink />', () => {
+  it('should render', () => {
+    renderTheme(<LogoLink>Children</LogoLink>);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
